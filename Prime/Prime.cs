cde8315e7ef4bf35bc3fa581c45f6bf6a1d45859
Laurie Waller <lauriewaller7@gmail.com>
2021-04-18 21:10:42 -0700

    remove comments

--- Prime/Prime.cs
+++ Prime/Prime.cs
@@ -1,7 +1,5 @@
 using System;
 using Prime.Models;
-using System.Collections.Generic;
-
 
 namespace Prime 
 {
@@ -16,30 +14,6 @@ namespace Prime
       Console.WriteLine("Here are the prime numbers");
       Console.WriteLine("smaller than or equal to " + input);
       PrimeNumbers.PrimeMethod(input);
-
-      
-      
-      // List<int> primeList = new List<int> {}; 
-    
-      // for (int index = 2; index <= 10; index++)
-      // {
-      //   primeList.Add(index);
-      //   Console.WriteLine("index is: " + index);
-      // }
-
-      // for (int index = 0; index < primeList.Count; index++)
-      // {
-      //   Console.WriteLine("number is: " + primeList[index]);
-      // }
-
-      
-
- 
-
-      // foreach (int index in primeList) 
-      // {
-      // Console.WriteLine("Index is: ", primeList[index]);
-      // }
     }   
   }
 }
